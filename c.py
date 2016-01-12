8d28db93e4e45266fa8cf6b031a91d500a90e71a
Michael Palmos <toqoz@hotmail.com>
2016-01-13 01:25:27 +1000

    reset

--- c.py
+++ c.py
@@ -1,8 +1,6 @@
 from configparser import RawConfigParser
 from os.path import expanduser
 
-#we are on master here
-
 parser = RawConfigParser()
 parser.read(expanduser('~') + '/.config/yarn/config')
 
