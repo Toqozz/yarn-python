9fbd541f47281b1e1d0bf6dd42cc630b1b392f2c
Michael Palmos <toqoz@hotmail.com>
2016-01-14 21:41:43 +1000

    change name to yarn

--- scripts/url-open.sh
+++ scripts/url-open.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# This script does not work currently.
 
 #Get the latest notification displayed and negate the loopback_distance option.
-var=`cat /tmp/dthread-log | tail -1 | awk -F'         ' '{ print $1 }'` #Split the strings if they have been looped, only use the first one.
+var=`cat /tmp/yarn-log | tail -1 | awk -F'         ' '{ print $1 }'` #Split the strings if they have been looped, only use the first one.
 #When something starts with 'http', send it to a new line.  After this, remove text after the end of the word/url.
 link=`echo $var | grep http | sed 's/http/\nhttp/g' | grep ^http | sed 's/\(^http[^ <]*\)\(.*\)/\1/g'`
 chromium $link      #Open chromium with the links
 
 #For a one-liner:
-#chromium $(cat /tmp/dthread-log | tail -1 | awk -F'         ' '{ print $1 }' | grep http | sed 's/http/\nhttp/g' | grep ^http | sed 's/\(^http[^ <]*\)\(.*\)/\1/g')
+#chromium $(cat /tmp/yarn-log | tail -1 | awk -F'         ' '{ print $1 }' | grep http | sed 's/http/\nhttp/g' | grep ^http | sed 's/\(^http[^ <]*\)\(.*\)/\1/g')
